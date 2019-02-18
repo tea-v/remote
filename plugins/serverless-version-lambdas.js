949d3194d53c14a668fe455da9a186b116ea916f
etienne_tripier <etienne.tripier@airbnb.com>
2019-02-18 01:30:53 -0800

    Fixed lambda versioning for CloudFront

---
+++ plugins/serverless-version-lambdas.js
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const _ = require('lodash');
+
+function getAssociations(resources) {
+  const distributions = _.pickBy(resources, {
+    Type: 'AWS::CloudFront::Distribution',
+  });
+  return _.flatMap(distributions, (distribution) => {
+    const associations = _.get(
+      distribution,
+      'Properties.DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations',
+      []
+    );
+    const cacheBehaviors = _.get(
+      distribution,
+      'Properties.DistributionConfig.CacheBehaviors',
+      []
+    );
+    return _.concat(
+      associations,
+      _.flatMap(cacheBehaviors, (cacheBehavior) =>
+        _.get(cacheBehavior, 'LambdaFunctionAssociations', [])
+      )
+    );
+  });
+}
+
+function getVersion(resources, arn) {
+  const key = _.findKey(resources, {
+    Type: 'AWS::Lambda::Version',
+    Properties: {
+      FunctionName: {
+        Ref: arn,
+      },
+    },
+  });
+  return key
+    ? {
+        'Fn::Join': [
+          '',
+          [
+            { 'Fn::GetAtt': [arn, 'Arn'] },
+            ':',
+            { 'Fn::GetAtt': [key, 'Version'] },
+          ],
+        ],
+      }
+    : undefined;
+}
+
+class VersionLambdas {
+  constructor(serverless, options) {
+    this.hooks = {
+      'before:package:finalize': this.updateLambdaVersion.bind(this),
+    };
+    this.options = options;
+    this.serverless = serverless;
+  }
+
+  updateLambdaVersion() {
+    const compiledResources = this.serverless.service.provider
+      .compiledCloudFormationTemplate.Resources;
+    const resources = this.serverless.service.resources.Resources;
+    const associations = getAssociations(resources);
+    _.forEach(associations, (association) => {
+      const arn = association.LambdaFunctionARN;
+      const version = getVersion(compiledResources, arn);
+      if (arn && version) {
+        // eslint-disable-next-line no-param-reassign
+        association.LambdaFunctionARN = version;
+      }
+    });
+  }
+}
+
+module.exports = VersionLambdas;
